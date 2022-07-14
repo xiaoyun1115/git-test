c5b1bdebf1d24bcb8e2b4b074ed3fd336e88ca1b
yun <1737825818@qq.com>
2022-07-14 15:02:52 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,7 +8,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 }
